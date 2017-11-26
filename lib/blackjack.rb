b587fc44b5e86cec3a1293babd3471569145f674
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-26 20:01:25 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,7 +49,7 @@ end
 def runner
   welcome
   initial_round
-  # card_total = card_total.to_i + initial_round.to_i
+  card_total = initial_round.to_i
 
   until card_total > 21
     hit?(card_total)
